451898e032dc1a85ade7502481a4c0265bb57fe1
Jordan Kline <jordan.kline@gmail.com>
2018-10-09 15:07:28 -0700

    added profile_url to serializer for profile picture integration

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :last_name, :email, :location, :total_time
+  attributes :id, :name, :last_name, :email, :location, :total_time, :profile_url
   # has_many :meditation_sessions
 end
